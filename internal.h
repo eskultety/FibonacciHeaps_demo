ca3328c0d6ea7c75031ea9dee040fb83cb644559
Erik Skultety <skultety.erik@yahoo.com>
2014-11-29 15:01:16 +0100

    internal: add common structures and macros

---
+++ internal.h
@@ -0,0 +1,24 @@
+#ifndef __INTERNAL__
+#define __INTERNAL__
+
+#define SWAP(x, y) _swap(&x, &y) 
+
+template <typename T>
+void _swap (T *a, T *b) {
+    T *tmp = a;
+    *a = *b;
+    *b = *tmp;
+}
+
+typedef struct FibNode *FibNodePtr;
+
+struct FibNode {
+    FibNodePtr parent;
+    FibNodePtr child;   // children doubly-linked list
+    FibNodePtr left, right;  // siblings doubly-linked list
+    int key;
+    unsigned degree;    // number of children
+    bool mark;          // whether X has lost child since X was made a child
+};
+
+#endif // __INTERNAL__
