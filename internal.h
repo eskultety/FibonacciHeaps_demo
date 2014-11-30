4258e0d6f235ab8abb1fcab5340803780b9a1083
Erik Skultety <skultety.erik@yahoo.com>
2014-11-30 01:18:22 +0100

    internal: introduced new structures and types as well as some tweaks
    Unfortunately we need to store the id of a node in the structure itself,
    because Prim's algorithm indexes the adjacency list by node id and
    searches a particular node by its id in the priority queue which in case
    of our former Fibonacci node definition would not be possible to do

--- internal.h
+++ internal.h
@@ -1,24 +1,39 @@
 #ifndef __INTERNAL__
 #define __INTERNAL__
 
-#define SWAP(x, y) _swap(&x, &y) 
+#include <climits>
+#include <vector>
+#define SWAP(x, y) _swap(&x, &y)
 
 template <typename T>
 void _swap (T *a, T *b) {
+    if (*a == *b)
+        return;
     T *tmp = a;
     *a = *b;
     *b = *tmp;
 }
 
 typedef struct FibNode *FibNodePtr;
+typedef struct AdjItem *AdjItemPtr;
+typedef std::vector<AdjItemPtr> *AdjListPtr;    // adjacency list
+typedef class FibonacciHeap FibonacciHeapPtr;
+
+class FibonacciHeap;
 
 struct FibNode {
+    unsigned id;
     FibNodePtr parent;
     FibNodePtr child;   // children doubly-linked list
     FibNodePtr left, right;  // siblings doubly-linked list
     int key;
     unsigned degree;    // number of children
     bool mark;          // whether X has lost child since X was made a child
+};
+
+struct AdjItem {
+    FibNodePtr data;
+    AdjItemPtr next;
 };
 
 #endif // __INTERNAL__
