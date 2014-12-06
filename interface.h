111310fdd2915757d0842e54f40e2c80e096e4d1
Erik Skultety <skultety.erik@yahoo.com>
2014-12-06 14:34:00 +0100

    interface: added mutexes which are dependent on the GUI

--- interface.h
+++ interface.h
@@ -1,3 +1,8 @@
+#ifndef __INTERFACE_H__
+#define __INTERFACE_H__
+
+#define MILLISEC 1000 // 1 second
+#define MUTEX_AC(s, p) _mutexAcquire(&s, &p);
 
 extern unsigned char speed; // algorithm speed limiter
 extern bool ready;  // signalizes if GUI ready for next update
@@ -14,3 +19,13 @@ typedef enum
 } sigTypes;
 
 void sigStepFinished();
+
+static inline void _mutexAcquire(unsigned char *s, bool *p)
+{
+    shared_mtx.lock();
+        *s = speed;
+        *p = pause_execution;
+    shared_mtx.unlock();
+}
+
+#endif // __INTERFACE_H__
